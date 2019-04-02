c94f9b2ca78dc19acecede8a4486c310e74790a1
Samuel Tian <samuel.tian2003@gmail.com>
2019-04-01 21:10:14 -0500

    Update cowpatibility

--- USACO/2018_19/DEC18/cowpatibility/cowpatibility.cpp
+++ USACO/2018_19/DEC18/cowpatibility/cowpatibility.cpp
@@ -1,41 +1,81 @@
-#include <iostream>
-#include <climits>
-#include <set>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define endl '\n'
-#define INF INT_MAX
 #define MAXN 50000
 
+struct Subset {
+	long long flavors[5] {0, 0, 0, 0, 0};
+};
+
+bool operator < (const Subset& a, const Subset& b) {
+	for (long long i = 0; i < 5; ++i) {
+		if (a.flavors[i] < b.flavors[i]) return true;
+		else if (a.flavors[i] > b.flavors[i]) return false;
+	}
+	return false;
+}
+
+long long n;
+Subset favorites[MAXN];
+map<Subset, long long> commonSubsets;
+
+Subset generateSubsets(long long s, long long x) {
+	Subset ret;
+	long long counter = 0;
+	for (long long i = 0; i < 5; ++i) {
+		if ((1 << i) & x) {
+//			cout << "PASS" << endl;
+			ret.flavors[counter] = favorites[s].flavors[i];
+			++counter;
+		}
+	}
+//	if (x == 3) {
+//		for (long long i = 0; i < 5; ++i) {
+//			cout << ret.flavors[i] << " ";
+//		}
+//		cout << endl;
+//	}
+
+	return ret;
+}
+
 int main() {
+//	freopen("input.txt", "r", stdin);
 	freopen("cowpatibility.in", "r", stdin);
 	freopen("cowpatibility.out", "w", stdout);
-	int n;
 	cin >> n;
-	int flavors[n][5];
-	/*int current = 0;
-	set<int> used;*/
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			cin >> flavors[i][j];
+	for (long long i = 0; i < n; ++i) {
+		for (long long j = 0; j < 5; ++j) {
+			cin >> favorites[i].flavors[j];
+		}
+		sort(favorites[i].flavors, favorites[i].flavors + 5);
+		for (long long j = 1; j <= 31; ++j) {
+			Subset s = generateSubsets(i, j);
+//			long long size = 0;
+//			while (size < 5 && s.flavors[size] != 0) ++size;
+//			for (long long k = 0; k < size; ++k) {
+//				cout << s.flavors[size] << " ";
+//			}
+//			cout << endl;
+			++commonSubsets[s];
 		}
 	}
-	int counter = 0;
-	for (int i = 0; i < n; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			set<int> merge;
-			for (int k = 0; k < 5; ++k) {
-				merge.insert(flavors[i][k]);
-			}
-			for (int k = 0; k < 5; ++k) {
-				if (merge.find(flavors[j][k]) != merge.end()) {
-					break;
-				}
-				if (k == 4) ++counter;
-			}
-		}
+
+	long long total = 0;
+	for (auto s : commonSubsets) {
+		long long size = 0;
+		while (size < 5 && s.first.flavors[size] != 0) ++size;
+		if (size == 0) continue;
+//		cout << "set: ";
+//		for (long long i = 0; i < size; ++i) {
+//			cout << s.first.flavors[i] << " ";
+//		}
+//		cout << endl << s.second << endl;
+		total += (s.second * (s.second - 1)) / 2 * (size % 2 == 0 ? -1 : 1);
 	}
-	cout << counter << endl;
+//	cout << total << endl;
+	cout << (n * (n - 1)) / 2 - total << endl;
+	return 0;
 }
