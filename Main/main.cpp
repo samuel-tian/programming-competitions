7f244d80ab916b5abd1af06597b53e01b86e1796
Samuel <samuel.tian2003@gmail.com>
2020-08-20 18:33:16 -0500

    update

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-#include <fstream>
-#include <climits>
-#include <vector>
-#include <utility>
-#include <random>
-#include <chrono>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef pair<int, int> pi;
+typedef pair<long long, long long> pll;
 typedef pair<pair<int, int>, int> ppi;
 typedef pair<int, pair<int, int> > pip;
 typedef vector<int> vi;
 typedef vector<long long> vll;
 typedef vector<pair<int, int> > vpi;
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define f first
 #define s second
@@ -62,104 +62,46 @@ struct chash {
 };
 
 const int N = 200005;
+const int M = 200005;
 const int Q = 200005;
 const int LOGN = 20;
 
-int n, q;
-int nex[N], tnex[LOGN][N]; // successor, successor in tree
-vi adj[N];
-int suc[LOGN][N];
-int comp[N], tr[N], dep[N], rn[N], cycsz[N]; // component label, tree label, depth in tree, root number, cycle size
-bool rt[N], vis[N]; // is root, visited
+int n, m, q;
+int a[N], p[M], perm[N];
+vi loc[N];
+int prev[LOGN][N];
 
-void dfs(int a, int p, int c, int t) {
-    if (rt[a])
-        dep[a] = 0;
-    vis[a] = true;
-    comp[a] = c;
-    tr[a] = t;
-    TRAV (b, adj[a]) {
-        if (b == p || rt[b])
-            continue;
-        dep[b] = dep[a] + 1;
-        dfs(b, a, c, t);
+int get(vi& v, int k) {
+    int lo = 0, hi = v.size()-1;
+    while (lo < hi) {
+        int mid = (lo + hi + 1) / 2;
+        if (v[mid] >= k)
+            hi = mid - 1;
+        else
+            lo = mid;
     }
+    return lo;
 }
 
 int main() {
 	chrono::high_resolution_clock::time_point t0 = chrono::high_resolution_clock::now();
 
 	setIO();
-    cin >> n >> q;
+    cin >> n >> m >> q;
     FOR (i, 0, n) {
-        cin >> nex[i];
-        nex[i]--;
-        tnex[0][i] = nex[i];
-        adj[nex[i]].pb(i);
+        cin >> p[i];
+        p[i]--;
+        loc[p[i]].pb(i);
     }
-    int c = 0;
+    FOR (i, 0, m) {
+        cin >> a[i];
+        a[i]--;
+    }
+    FOR (i, 0, n)
+        sort(loc[i].begin(), loc[i].end());
     FOR (i, 0, n) {
-        if (vis[i])
-            continue;
-        int cur = i;
-        while (!vis[cur]) {
-            vis[cur] = true;
-            cur = nex[cur];
-        }
-        vi roots;
-        while (roots.empty() || cur != roots[0]) {
-            rt[cur] = true;
-            rn[cur] = roots.size();
-            tnex[0][cur] = cur;
-            roots.pb(cur);
-            cur = nex[cur];
-        }
-        FOR (i, 0, roots.size()) {
-            cycsz[roots[i]] = roots.size();
-            dfs(roots[i], -1, c, roots[i]);
-        }
-        c++;
-    }
-    FOR (i, 1, LOGN) {
-        FOR (j, 0, n) {
-            tnex[i][j] = tnex[i-1][tnex[i-1][j]];
-        }
-    }
-    FOR (i, 0, q) {
-        int a, b;
-        cin >> a >> b;
-        a--;
-        b--;
-        int ans = 0;
-        if (comp[a] != comp[b])
-            ans = -1;
-        else {
-            if (tr[a] == tr[b]) { // same tree
-                if (dep[a] < dep[b])
-                    ans = - 1;
-                else {
-                    int dif = dep[a] - dep[b];
-                    FOR (j, 0, LOGN) {
-                        if (dif & (1<<j)) {
-                            a = tnex[j][a];
-                        }
-                    }
-                    if (a == b)
-                        ans = dif;
-                    else
-                        ans = -1;
-                }
-            }
-            else {
-                if (!rt[b])
-                    ans = -1;
-                else {
-                    int art = tr[a];
-                    ans = dep[a] + (rn[b] - rn[art] + cycsz[art]) % cycsz[art];
-                }
-            }
-        }
-        cout << ans << '\n';
+
+        prev[0][i] = 
     }
 
 	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
--- Main/template.cpp
+++ Main/template.cpp
@@ -9,6 +9,7 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 typedef pair<int, int> pi;
+typedef pair<long long, long long> pll;
 typedef pair<pair<int, int>, int> ppi;
 typedef pair<int, pair<int, int> > pip;
 typedef vector<int> vi;
@@ -33,10 +34,18 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 #define min3(a, b, c) min(min((a), (b)), (c))
 
 void setIO(string name = "") {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	if (name == "") return;
-	if (name == "input") {freopen("input.txt","r",stdin);}
-	else {freopen((name+".in").c_str(),"r",stdin); freopen((name+".out").c_str(),"w",stdout);}
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	if (name == "")
+        return;
+    if (name == "input") {
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    }
+	else {
+        freopen((name+".in").c_str(),"r",stdin);
+        freopen((name+".out").c_str(),"w",stdout);
+    }
 }
 
 const int INF = INT_MAX;
@@ -46,7 +55,11 @@ const int MAXSEG = (1<<18);
 const int MUL = 1000001;
 const int MOD = 1000000007;
 const ll RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
-struct chash { ll operator()(ll x) const { return x ^ RANDOM; } };
+struct chash {
+    ll operator()(ll x) const {
+        return x ^ RANDOM;
+    }
+};
 
 int main() {
 	chrono::high_resolution_clock::time_point t0 = chrono::high_resolution_clock::now();
